6c8af37f762e92bcdeccacc14902054d33b58534
yutakikuchi51 <120287555+yutakikuchi51@users.noreply.github.com>
2023-09-25 22:44:47 -0600

    Week2 Assignment
    create a new Next.js project
    create a React component
    create a new page

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <main>
+            <h2>Yuta Kikuchi</h2>
+            <p>CPRG 306 E</p>
+            <Link href="https://github.com/yutakikuchi51/cprg306-assignments">My GitHub repository</Link>
+        </main>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo";
+
+export default function week2(){
+    return(
+        <main>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </main>
+    )
+}
